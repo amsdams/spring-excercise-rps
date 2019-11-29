9af320c27d0ccf78df945c7a912e3e771e2edcac
amsdams <julien.langeveld@gmail.com>
2019-11-29 14:43:00 +0100

    adding configmap?

--- src/main/java/com/study/chat/websocket/config/WebConfig.java
+++ src/main/java/com/study/chat/websocket/config/WebConfig.java
@@ -1,23 +1,20 @@
-package com.study.chat.websocket.config;
+/*package com.study.chat.websocket.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
 @Configuration
-@ComponentScan
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 
-		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST")
-				.allowedHeaders("Origin", "Accept", "Content-Type", "Authorization").allowCredentials(true)
-				.maxAge(3600);
+		registry.addMapping("/**");
 
 	}
 
 }
+*/
