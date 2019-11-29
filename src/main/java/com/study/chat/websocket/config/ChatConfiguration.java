6f86954cd0ed154a3c16f8c20e96c2e7f85ff00d
amsdams <julien.langeveld@gmail.com>
2019-11-29 13:35:10 +0100

    updating for openshift test

--- src/main/java/com/study/chat/websocket/config/ChatConfiguration.java
+++ src/main/java/com/study/chat/websocket/config/ChatConfiguration.java
@@ -1,23 +1,25 @@
 package com.study.chat.websocket.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-
 @Configuration
 @EnableWebSocketMessageBroker
 public class ChatConfiguration implements WebSocketMessageBrokerConfigurer {
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/chat", "/participants");
-        registry.setApplicationDestinationPrefixes("/app");
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.enableSimpleBroker("/chat", "/participants");
+
+		registry.setApplicationDestinationPrefixes("/app");
+	}
+
+	@Override
+    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+        stompEndpointRegistry.addEndpoint("/endpoint").setAllowedOrigins("*").withSockJS();
     }
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
-        stompEndpointRegistry.addEndpoint("/endpoint").withSockJS();
-    }
 }
